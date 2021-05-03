9167b5f97035b0ad7dee62563c48bb6c6bcfe8e7
boyer <asboyer@gmail.com>
2021-05-03 19:49:13 -0400

    new changes

--- generator.py
+++ generator.py
@@ -1,3 +1,3 @@
 from tools import generate
-password = generate()
+password = generate(readability=True, max_length=20)
 print(password)
--- tools.py
+++ tools.py
@@ -22,7 +22,18 @@ def choice(string):
     i = randint(0, len(string) - 1)
     return string[i]
 
+def shuffle(l):
+    l2 = []
+    while len(l) > 0:
+        c = choice(l)
+        l2.append(c)
+        l.remove(c)
+    return l2
+
 def generate(min_length=12, max_length=14, readability=False):
+    
+    # goal - generate a password with words, mixed in with a few special chars + nums
+
     length = randint(min_length, max_length)
     
     if readability:
@@ -33,43 +44,69 @@ def generate(min_length=12, max_length=14, readability=False):
 
         password = []
 
-        numSpecials = randint(1, length/(length/2))
-        numNums = randint(1, length/(length/2))
+        numSpecials = randint(1, 3) 
+        numNums = randint(1, 2)
 
-        remainingLength = length - numSpeicals - numNums
+        remainingLength = length - numSpecials - numNums
 
         specialsIn = ''
-        numsIn = ''
+        numsIn = '' #
 
         for i in range(numNums):
-            numsIn += choice(nums)
+            numsIn += choice(nums) #
         for i in range(numSpecials):
             specialsIn += choice(special)
 
-        firstNum = randint(2, remainingLength)
-        secondNum = remainingLength - firstNum
+        firstLen = randint(2, remainingLength - 3)
+        secondLen = remainingLength - firstLen
 
-        firstWord = words.get_random_word(hasDictionaryDef="true", minLength=firstNum, maxLength=firstNum)
-        secondWord = words.get_random_word(hasDictionaryDef="true", minLength=secondNum, maxLength=secondNum)
+        firstWord = None
+        secondWord = None
+
+        while firstWord == None or secondWord == None:
+            firstWord = words.get_random_word(hasDictionaryDef="true", minLength=firstLen, maxLength=firstLen)
+            secondWord = words.get_random_word(hasDictionaryDef="true", minLength=secondLen, maxLength=secondLen)
+
+        print(f'First word: {firstWord}')
+        print(f'Second word: {secondWord}')
 
         newFirst = ''
         newSecond = ''
 
-        for char in newFirst:
+
+        # make a memify
+        for char in firstWord:
             cap = randint(0, 1)
             if cap == 0:
                 newFirst += char
             else:
                 newFirst += char.upper()
 
-        for char in newFirst:
+        for char in secondWord:
             cap = randint(0, 1)
             if cap == 0:
-                newFirst += char
+                secondFi += char
             else:
                 newFirst += char.upper()
 
-        password = [numsIn, specialsIn]
+        for char in numsIn + specialsIn: # 
+            password.append(char)
+
+        password.append(firstWord)
+        password.append(secondWord)
+
+        print(f'Before shuffling: {password}')
+        
+        password = shuffle(password)
+
+        print(f'After shuffling: {password}')
+
+        finalPass = ''
+        for st in password:
+            finalPass += st
+
+        return finalPass.replace(' ', '').replace('.', '_')   
+
 
 
     if not readability:
