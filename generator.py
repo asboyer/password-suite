066963324b12e650d408e6c616fd623095bdd11d
boyer <asboyer@gmail.com>
2021-05-10 19:26:17 -0400

    ended generator for now started manager

--- generator.py
+++ generator.py
@@ -1,4 +1,4 @@
 from boyer import clear
 from tools import generate
-password = generate(readability=True, max_length=20, difficulty=True)
-print(password)
+
+print(generate(readability=True, max_length=20, difficulty=True))
---
+++ manager.py
@@ -0,0 +1,11 @@
+# ideas:
+
+# store passwords
+    # use dictionary to store passwords
+    # use json file to store passwords
+# encrypt passwords
+    # a lot of ways to encrypt passwords
+        # it would be cool if we used our caesar cipher
+# give user accessibility
+    # command line experience
+
