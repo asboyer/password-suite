463d4962d7f5a448815cd58056fa2180081187ff
boyer <asboyer@gmail.com>
2021-05-17 22:45:04 -0400

    lesson

--- lesson.py
+++ lesson.py
@@ -1,39 +1,106 @@
+# # lists
+# l = [1, 2, 3, 4]
+
+# arr2D = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+# #         0  1  2
+# #            0          1            2
+
+# # tuples
+# tupl = (1, 2, 3)
+
+# #tupl[0]  tupl[1]  tupl[2]
+# (alek,    two,     three) = tupl
+
+# # sets
+
+# myset = {"apple", "banana", "cherry", "apple", "papaya"}
+
+# myset.add("orange")
+
+# alekset = {"pineapple", "mango", "watermelon", "dragonfruit", "papaya", "apple"}
+
+# # myset.update(alekset)
+# # ^             ^
+# # |             |______________ stays the same  
+# # the on that gets update (updates the set with the values from set in ())
+
+# # newset = myset.union(alekset)
+
+# # alekset.intersection_update(myset)
+
+# # anotherset = alekset.intersection(myset)
+
+# # alekset.symmetric_difference_update(myset)
+
+# lastset = symmetric_difference(myset)
+
+# print(alekset)
+
+
 # dictionaries
 
-# lists
-l = [1, 2, 3, 4]
+dictionary = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "electric": False,
+  "colors": ["red", "white", "blue"],
+  "owners": {
 
-arr2D = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
-#         0  1  2
-#            0          1            2
+        "Kent": {
+            
+            "address": "41 Bridge Road",
+            
+            "siblings": {
+                
+                "Amy": {
+                    "address": "411 Bridge Road",
+                },
+            
+            "car_price": 1900
+            
+            }
+        },
 
-# tuples
-tupl = (1, 2, 3)
+        "Alek": {
+            "address": "42 Bridge Road"
+        },
 
-#tupl[0]  tupl[1]  tupl[2]
-(alek,    two,     three) = tupl
+        "Andrew": {
+            "address": "Beverly Drive Road"
+        }
+  }
+}
 
-# sets
+# print(dictionary["brand"])
+# print(dictionary["year"])
+# print(dictionary["model"])
+# keys and values
 
-myset = {"apple", "banana", "cherry", "apple", "papaya"}
+# print(dictionary["owners"]["Andrew"].get("address"))
+# print('\n___________\n')
+# print(dictionary.get("owners").get("Andrew").get("address"))
 
-myset.add("orange")
+x = dictionary.keys()
 
-alekset = {"pineapple", "mango", "watermelon", "dragonfruit", "papaya", "apple"}
+for i in x:
+    print(i)
 
-# myset.update(alekset)
-# ^             ^
-# |             |______________ stays the same  
-# the on that gets update (updates the set with the values from set in ())
 
-# newset = myset.union(alekset)
-
-# alekset.intersection_update(myset)
-
-# anotherset = alekset.intersection(myset)
-
-# alekset.symmetric_difference_update(myset)
-
-lastset = symmetric_difference(myset)
-
-print(alekset)
+stars = {
+    "Darth Vader": {
+        "lightsaber-color": "red",
+        "side": "sith",
+        "movies": [1, 2, 3, 4, 5, 6]
+    }
+    "Darth Vader": {
+        "lightsaber-color": "red",
+        "side": "sith",
+        "movies": [1, 2, 3, 4, 5, 6]
+    }
+    "Darth Vader": {
+        "lightsaber-color": "red",
+        "side": "sith",
+        "movies": [1, 2, 3, 4, 5, 6]
+    }
+}
